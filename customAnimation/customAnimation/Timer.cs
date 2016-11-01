3ba96ef2652d8af3050ee471e23587fb3045889a
Scott Predmore <spredmore02@gmail.com>
2016-10-31 20:44:03 -0400

    Fixing bug around Timer - 2

--- customAnimation/customAnimation/Timer.cs
+++ customAnimation/customAnimation/Timer.cs
@@ -6,58 +6,67 @@ using Microsoft.Xna.Framework;
 
 namespace customAnimation
 {
-    class Timer
-    {
-        private float elapsedTime = 0f;         // Total time the timer has been running.
-        public float timerEndTime;              // End time for the timer. Stop when elapsedTime is equal to timerEndTime.
-        private bool isTimerRunning = false;      // Stores if the timer is running or not.
-        private bool isTimerCompleted = false;    // Stores if the timer has completed or not.
+	class Timer
+	{
+		private float elapsedTime = 0f;				// Total time the timer has been running.
+		private float timerEndTime;					// End time for the timer. Stop when elapsedTime is equal to timerEndTime.
+		private bool isTimerRunning = false;		// Stores if the timer is running or not.
+		private bool isTimerCompleted = false;		// Stores if the timer has completed or not.
 
-        public float ElapsedTime
-        {
-            get { return elapsedTime; }
-        }
+		public float ElapsedTime
+		{
+			get { return elapsedTime; }
+		}
 
-        public bool TimerStarted
-        {
+		public bool TimerStarted
+		{
 			get { return isTimerRunning; }
-        }
+		}
 
-        public bool TimerCompleted
-        {
+		public bool TimerCompleted
+		{
 			get { return isTimerCompleted; }
-        }
+		}
 
-        public Timer(float endTime)
-        {
-            timerEndTime = endTime;
+		public Timer(float endTime)
+		{
+			timerEndTime = endTime;
 			isTimerRunning = false;
 			isTimerCompleted = false;
-        }
+		}
 
-        public void startTimer()
-        {
+		public void startTimer()
+		{
 			isTimerRunning = true;
 			isTimerCompleted = false;
-        }
+		}
 
-        public void stopTimer()
-        {
+		public void stopTimer()
+		{
 			isTimerRunning = false;
-            elapsedTime = 0;
-        }
+			elapsedTime = 0;
+		}
 
-        public void Update(GameTime gameTime)
-        {
-			if (isTimerRunning && elapsedTime < timerEndTime)
-            {
-                elapsedTime += (float)gameTime.ElapsedGameTime.TotalSeconds;
-            }
-			else if (isTimerRunning)
-            {
-				isTimerRunning = false;
-				isTimerCompleted = true;
-            }   
-        }
-    }
+		public void resetTimer()
+		{
+			isTimerRunning = false;
+			isTimerCompleted = false;
+			elapsedTime = 0;
+		}
+
+		public void Update(GameTime gameTime)
+		{
+			if (isTimerRunning)
+			{
+				if (elapsedTime < timerEndTime)
+				{
+					elapsedTime += (float)gameTime.ElapsedGameTime.TotalSeconds;
+				}
+				else
+				{
+					isTimerCompleted = true;
+				}
+			} 
+		}
+	}
 }
